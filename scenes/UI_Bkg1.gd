487813d8984f876664e61a14105c26f6b01bfc5c
Óscar A. Montiel <etherealflux94@gmail.com>
2020-02-02 17:38:55 -0600

    Ya funciona con diccionarios.

--- scenes/UI_Bkg1.gd
+++ scenes/UI_Bkg1.gd
@@ -2,17 +2,46 @@ extends Control
 
 #Variable global porque necesitamos acceso para todo el programa.
 var player_words = []
-var prompts = ["un nombre", "un sustantivo", "un adverbio", "un adjetivo"]
-var storia = "Me llamo %s. Soy un %s y mato %s. Valgo %s."
+#Para usar un diccionario se usan llaves, en lugar de corchetes que son para 
+#arreglos.
+var template = [
+		{
+		#Diccionario 1
+		"prompts": ["un nombre", "un sustantivo", "un adverbio", "un adjetivo"],
+		"story": "Tu nombre es %s. Eres un %s. Coges %s. Vales %s."
+		},
+		#Diccionario 2
+		{
+		"prompts": ["zebra", "lagarto", "pene", "vagina"],
+		"story": "Sexo %s anal y %s en %s. %s"
+		},
+		#Diccionario 3
+		{
+		"prompts": ["cebolla", "artes", "pito", "México"],
+		"story": "Jajaja %s jojojo %s en %s. %s"
+		}
+	]
+	
+var current_story
+	
+
 #onready sirve para avisarle a Godot que esta variable existirá más abajito.
 onready var PlayerText = $VBoxContainer/HBoxContainer/playerText
 onready var DisplayText = $VBoxContainer/guion1
 
 func _ready():
+	pick_current_story()
 	DisplayText.text = "Bienvenido, puto."
 	#Enfoca la caja de input.
 	PlayerText.grab_focus()
 	check_player_words_length()
+
+func pick_current_story():
+	#randi() debe "modularse" con un número. Comienza desde 0; si queremos que
+	#comience desde 1, le sumamos un 1 al número.
+	randomize() #randomize se aproxima al azar (para que no salga lo mismo 
+	#siempre }).
+	current_story = template[ randi() % template.size() ]
 
 func add_to_player_words():
 	player_words.append(PlayerText.text)
@@ -22,7 +51,7 @@ func add_to_player_words():
 
 func is_story_done():
 	#nada corre debajo de return.
-	return player_words.size() == prompts.size()
+	return player_words.size() == current_story.prompts.size()
 
 func check_player_words_length():
 	if is_story_done():
@@ -31,10 +60,10 @@ func check_player_words_length():
 		prompt_player()
 
 func tell_story():
-	DisplayText.text = storia % player_words
+	DisplayText.text = current_story.story % player_words
 
 func prompt_player():
-	DisplayText.text += "Podrías darme un"+prompts[player_words.size()] + ",pofabó."
+	DisplayText.text += "Podrías darme un"+current_story.prompts[player_words.size()] + ",pofabó."
 
 func _on_TextureButton_pressed():
 	print("Botón presionado")
@@ -45,7 +74,7 @@ func _on_TextureButton_pressed():
 	else:
 		add_to_player_words()
 
-func _on_playerText_text_entered(new_text):
+func _on_playerText_text_entered(_new_text):
 	add_to_player_words()
 
 func end_game():
