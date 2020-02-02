3162dc1ce8a4b33710f52504d611a3dd8397fb6f
Óscar A. Montiel <etherealflux94@gmail.com>
2020-02-02 12:52:52 -0600

    Arreglos

--- scenes/UI_Bkg1.gd
+++ scenes/UI_Bkg1.gd
@@ -2,27 +2,25 @@ extends Control
 
 #Variable global porque necesitamos acceso para todo el programa.
 var player_words = []
-var nombres = ["Óscar", "Ita", "Jasso"]
-var storia = "Beso mortal entre %s y %s. Desprecio total de %s."
+var prompts = ["un nombre", "un sustantivo", "un adverbio", "un adjetivo"]
+var storia = "Me llamo %s. Soy un %s y mato %s. Valgo %s."
 #onready sirve para avisarle a Godot que esta variable existirá más abajito.
 onready var PlayerText = $VBoxContainer/HBoxContainer/playerText
 onready var DisplayText = $VBoxContainer/guion1
 
-#func _ready():
-#	DisplayText.text = storia % nombres
-	
-func _on_playerText_text_entered(new_text):
-	add_to_player_words(new_text)
+func _ready():
+	DisplayText.text = "Bienvenido, puto."
+	check_player_words_length()
 
-func _on_TextureButton_pressed(new_words):
-	add_to_player_words(new_words)
-
-func add_to_player_words(new_words):
+func add_to_player_words():
 	player_words.append(PlayerText.text)
+	DisplayText.text = ""
+	PlayerText.clear()
+	check_player_words_length()
 
 func is_story_done():
 	#nada corre debajo de return.
-	return player_words.size() == nombres.size()
+	return player_words.size() == prompts.size()
 
 func check_player_words_length():
 	if is_story_done():
@@ -31,7 +29,17 @@ func check_player_words_length():
 		prompt_player()
 
 func tell_story():
-	DisplayText.text = storia % nombres 
+	DisplayText.text = storia % player_words
 
 func prompt_player():
-	DisplayText.text = "May I have "+nombres[player_words.size()] + "pofabó."
+	DisplayText.text += "Podrías darme un"+prompts[player_words.size()] + ",pofabó."
+
+
+func _on_TextureButton_pressed():
+	add_to_player_words()
+	print("Botón presionado")
+
+
+
+func _on_playerText_text_entered(new_text):
+	add_to_player_words()
